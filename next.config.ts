561260eafb444fce0ada2eae315c033d56c9dd9c
Amir Reza Asadi <asadiaa@mail.uc.edu>
2025-07-10 17:40:33 -0400

    feat: add ignore-loader to exclude test files from pdf-parse during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    // Exclude test files from pdf-parse during build
+    config.module.rules.push({
+      test: /node_modules\/pdf-parse\/test\//,
+      loader: 'ignore-loader'
+    });
+    
+    return config;
+  },
+  serverExternalPackages: ['pdf-parse']
 };
 
 export default nextConfig;
